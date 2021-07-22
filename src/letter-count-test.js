dd98f68fcd196df7c3a80ac4f2cda901d14f0074
Jeff Ripke <jripke74@gmail.com>
2021-07-22 07:58:00 -0700

    add test logic

--- src/letter-count-test.js
+++ src/letter-count-test.js
@@ -1,3 +1,12 @@
 'use strict';
 
+const { expect } = require('chai');
+const { getLetterCount } = require('./letter-count');
 
+describe('getLetterCount - basic functionality', () => {
+  it('returns an empty object when passed an empty string', () => {
+    const expected = {};
+    const actual = getLetterCount('');
+    expect(actual).to.deep.equal(expected);
+  });
+});
